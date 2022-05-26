0f2d99a99e4d3a2154675d748716134066ee65ea
Simona <simona.snapk@gmail.com>
2022-05-25 21:44:52 -0700

    setting up the base of the post model

--- models/post.js
+++ models/post.js
@@ -1,5 +1,36 @@
 const { Model, DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
 class Post extends Model { }
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [1, 36]
+            },
+        },
+        content: {
+            type: DataTypes.TEXT('tiny'),
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "post"
+    }
+);
+
+module.exports = Post;
