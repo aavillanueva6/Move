b8ce62d323f3b6b8b53924acf5e4aca2e1f20288
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-06-01 20:47:50 -0700

    adds socket formatting to css, updates server for socket

--- server.js
+++ server.js
@@ -47,7 +47,19 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
+
+const users = [];
 io.on('connection', (socket) => {
+  // socket.on('user_join', (data) => {
+  const userID = Math.random();
+  const socketID = socket.id;
+  const newUser = {
+    socketID,
+    userID,
+  };
+  users.push(newUser);
+  console.log(users);
+  // });
   console.log('a user connected');
   socket.broadcast.emit('hi');
   socket.on('disconnect', () => {
