745b890e79687ddf2342c8a5f12389a5c36d646f
Niccatania <Niccatania6@gmail.com>
2022-06-01 19:49:26 -0700

    handlebars minor corrections, adding asset folder

--- public/js/profile.js
+++ public/js/profile.js
@@ -17,7 +17,7 @@ const newFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to create project');
+        alert('Failed to create post');
       }
     }
   };
@@ -26,14 +26,14 @@ const newFormHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/projects/${id}`, {
+      const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE',
       });
   
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to delete project');
+        alert('Failed to delete post');
       }
     }
   };
