05f9d38aa21250875e3fe53bab89cc29aba291f4
Niccatania <Niccatania6@gmail.com>
2022-06-04 00:34:27 -0700

    GOing to pull

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
-const { Model, DataTypes } = require('sequelize');
+const { Sequelize, Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+
 
 class User extends Model {
   checkPassword(loginPw) {
