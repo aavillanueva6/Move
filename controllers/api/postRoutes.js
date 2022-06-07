3ea57620d7f48c73f929a02c301204c7e440214d
Niccatania <Niccatania6@gmail.com>
2022-06-06 21:10:08 -0700

    update

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,48 +2,43 @@ const router = require('express').Router();
 const { Post, Comment } = require('../../models');
 
 // create new post
-router.post('/', async (req, res) =>
-{
-    try
-    {
-        req.body.user_id = req.session.user_id;
-        const postData = await Post.create(req.body);
-        res.status(200).json(postData);
-    } catch (err)
-    {
-        res.status(500).json(err);
-    }
+router.post('/', async (req, res) => {
+  try {
+    req.body.user_id = req.session.user_id;
+    const postData = await Post.create(req.body);
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // update existing post
-router.put('./:id', async (req, res) =>
-{
-    try
-    {
-        const postData = await Post.findByPk(req.params.id);
+router.put('./:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
 
-        if (!postData)
-        {
-            res.status(404).json({ message: 'No post found with that id' });
-            return;
-        }
-
-        const updatedPostData = await Post.update({
-            title: req.body.title,
-            content: req.body.content
-        },
-            {
-                where: {
-                    id: req.params.id,
-                    user_id: req.session.user_id
-                }
-            });
-
-        res.status(200).json(updatedPostData);
-    } catch (err)
-    {
-        res.status(500).json(err);
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with that id' });
+      return;
     }
+
+    const updatedPostData = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
+
+    res.status(200).json(updatedPostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //id
@@ -54,43 +49,36 @@ router.put('./:id', async (req, res) =>
 //category_id
 
 // delete an existing post
-router.delete('/:id', async (req, res) =>
-{
-    try
-    {
-        const response = await Post.destroy({
-            where: {
-                id: req.params.id,
-                user_id: req.session.user_id
-            }
-        });
+router.delete('/:id', async (req, res) => {
+  try {
+    const response = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
 
-        if (!response)
-        {
-            res.status(404).json({ message: 'No post found with that id' });
-        }
-
-        res.status(200).json(response);
-    } catch (err)
-    {
-        res.status(500).json(err);
+    if (!response) {
+      res.status(404).json({ message: 'No post found with that id' });
     }
+
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create a new comment on a post
-router.post('/:id/comment', async (req, res) =>
-{
-    try
-    {
-        req.body.user_id = req.session.user_id;
-        req.body.post_id = req.params.id;
+router.post('/:id/comment', async (req, res) => {
+  try {
+    req.body.user_id = req.session.user_id;
+    req.body.post_id = req.params.id;
 
-        const commentData = await Comment.create(req.body);
-        res.status(200).json(commentData);
-    } catch (err)
-    {
-        res.status(500).json(err);
-    }
+    const commentData = await Comment.create(req.body);
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,125 +4,124 @@ const withAuth = require('../utils/auth');
 
 // renders homepage, limits posts to 20
 router.get('/', async (req, res) => {
-    try {
-        const postData = await Post.findAll({
-            limit: 20,
-            include: [
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ]
-        });
+  try {
+    const postData = await Post.findAll({
+      limit: 20,
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
 
-        const posts = postData.map((post) => post.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-        res.render('homepage', {
-            posts,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // renders page with posts filtered by categories
 router.get('/category/:id', async (req, res) => {
-    try {
-        const postData = await Post.findAll({
-            where: { category_id: req.params.id },
-            include: [
-                {
-                    model: User,
-                    attributes: ['username']
-                },
-                {
-                    model: Category
-                }
-            ]
-        });
+  try {
+    const postData = await Post.findAll({
+      where: { category_id: req.params.id },
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Category,
+        },
+      ],
+    });
 
-        const posts = postData.map((post) => post.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-        // TODO: render page name
-        res.render('category', {
-            posts,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    // TODO: render page name
+    res.render('category', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 
 // renders single post
 router.get('/post/:id', async (req, res) => {
-    try {
-        const postData = await Post.findByPk(req.params.id, {
-            include: [
-                {
-                    model: User,
-                    attributes: ['username']
-                },
-                {
-                    model: Comment,
-                    include: [User]
-                }
-            ]
-        });
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
 
-        const post = postData.get({ plain: true });
+    const post = postData.get({ plain: true });
 
-        // TODO: render page name
-        res.render('post', {
-            post,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    // TODO: render page name
+    res.render('post', {
+      post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // renders profile page, sends all posts created by current user
 router.get('/profile', withAuth, async (req, res) => {
-    try {
-        const postData = await Post.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ],
-            where: { user_id: req.session.user_id }
-        });
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+      where: { user_id: req.session.user_id },
+    });
 
-        const posts = postData.map((post) => post.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-        res.render('profile', {
-            posts,
-            logged_in: true
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    res.render('profile', {
+      posts,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // renders login page
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/profile');
-        return;
-    }
-    res.render('login');
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+  res.render('login');
 });
 
 // renders signup page
 router.get('/signup', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/profile');
-        return;
-    }
-    res.render('signup');
-})
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+  res.render('signup');
+});
 
 module.exports = router;
