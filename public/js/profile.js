a8d5790471ac4fc449617886d1a61757d56cc949
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-06-10 14:53:41 -0700

    fixes bug due to elements not found on page when script loads

--- public/js/profile.js
+++ public/js/profile.js
@@ -38,11 +38,10 @@ const delButtonHandler = async (event) => {
   }
 };
 
-document
-  .querySelector('.new-post-form')
-  .addEventListener('submit', newFormHandler);
+// document
+//   .querySelector('.new-post-form')
+//   .addEventListener('submit', newFormHandler);
 
-document
-  .querySelector('.post-list')
-
-  .addEventListener('click', delButtonHandler);
+// document
+//   .querySelector('.post-list')
+//   .addEventListener('click', delButtonHandler);
