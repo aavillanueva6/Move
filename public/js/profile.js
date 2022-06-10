a9485786ba90e67e77f107a986764ecb7f0c7775
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-06-10 16:29:03 -0700

    commented out entire code, I think it was causing a bug on page load

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,42 +1,42 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
+// const newFormHandler = async (event) => {
+//   event.preventDefault();
 
-  const name = document.querySelector('#post-name').value.trim();
+//   const name = document.querySelector('#post-name').value.trim();
 
-  const description = document.querySelector('#post-desc').value.trim();
+//   const description = document.querySelector('#post-desc').value.trim();
 
-  if (name && description) {
-    const response = await fetch('/api/posts', {
-      method: 'POST',
-      body: JSON.stringify({ name, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+//   if (name && description) {
+//     const response = await fetch('/api/posts', {
+//       method: 'POST',
+//       body: JSON.stringify({ name, description }),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create post');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert('Failed to create post');
+//     }
+//   }
+// };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute('data-id')) {
+//     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE',
-    });
+//     const response = await fetch(`/api/posts/${id}`, {
+//       method: 'DELETE',
+//     });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete post');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert('Failed to delete post');
+//     }
+//   }
+// };
 
 // document
 //   .querySelector('.new-post-form')
